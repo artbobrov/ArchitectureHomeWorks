0bd05ed33dc2f5f259b5a528890c013654c7c062
Liza <liza8361@yandex.ru>
2020-03-11 07:56:29 +0300

    done grep

--- bash/src/main/java/bash/CommandExecutor.java
+++ bash/src/main/java/bash/CommandExecutor.java
@@ -1,6 +1,12 @@
 package bash;
 
+import org.apache.commons.cli.*;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // CommandExecutor исполняет команды
 public class CommandExecutor {
@@ -27,6 +33,8 @@ public class CommandExecutor {
                 return executePwd(args);
             case ("exit"):
                 return executeExit(args);
+            case ("grep"):
+                return executeGrep(args, resultOfLastCommand, pipePosition);
             default:
                 String newCommand = command + " ";
                 if (args.length == 0 & pipePosition > 1) {
@@ -176,6 +184,127 @@ public class CommandExecutor {
         while ((line = reader.readLine()) != null) {
             result += line;
         }
+        return result;
+    }
+
+    // Выполняет команду grep. Если команда не первая в очереди pipe и у нее нет аргументов -- то
+    // в качестве входа ей подается результат предыдущей команды.
+    // Ключи -w, -i, -A могут стоять между аргументами в любом месте и в любом порядке.
+    // Возвращает её output в виде строки.
+    public String executeGrep(String[] args, String resultOfLastCommand, int pipePosition) {
+        String result = "";
+        Options options = new Options();
+        options.addOption("w", false, "Select  only  those  lines  containing  matches that form whole words");
+        options.addOption("i", false, "Ignore case");
+        options.addOption("A", true, "print n last lines after find line");
+        org.apache.commons.cli.CommandLine cmd;
+        CommandLineParser parserCL = new DefaultParser();
+        try {
+            cmd = parserCL.parse(options, args);
+        } catch (ParseException | NumberFormatException e) {
+            return "catch grep parse exception";
+        }
+        String stringToParse = "";
+        if (cmd.getArgs().length == 1 & pipePosition > 1) {
+            stringToParse = resultOfLastCommand;
+        } else {
+            if (cmd.getArgs().length != 2) {
+                return "error args for grep";
+            }
+            File file = new File(cmd.getArgs()[1]);
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(new FileReader(file));
+                String text = null;
+                while ((text = reader.readLine()) != null) {
+                    stringToParse += text;
+                    stringToParse += System.lineSeparator();
+                }
+            } catch (FileNotFoundException e) {
+                return "grep: file does not exist";
+            } catch (IOException e) {
+                return "catch IO exception";
+            } finally {
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                }
+            }
+        }
+
+        boolean ignoreCase = false;
+        boolean selectWords = false;
+        boolean printLastStrings = false;
+        int optArg = 0;
+
+        for (Option opt : cmd.getOptions()) {
+            if ("i".equals(opt.getOpt())) {
+                ignoreCase = true;
+            }
+            if ("w".equals(opt.getOpt())) {
+                selectWords = true;
+            }
+            if ("A".equals(opt.getOpt())) {
+                optArg = Integer.parseInt(opt.getValue());
+                printLastStrings = true;
+            }
+        }
+
+        String redex = cmd.getArgs()[0];
+
+        if (ignoreCase) {
+            stringToParse = stringToParse.toLowerCase();
+            redex = redex.toLowerCase();
+        }
+
+        Pattern pattern = Pattern.compile(redex);
+        Matcher matcher = pattern.matcher(stringToParse);
+        List<String> foundMatches = new ArrayList<String>();
+        if (matcher.find()) {
+            for (int i = 0; i <= matcher.groupCount(); i++) {
+                foundMatches.add(matcher.group(i));
+            }
+        }
+
+        String[] splitStrings = stringToParse.split(System.lineSeparator());
+        for (int i = 0; i < splitStrings.length; i++) {
+            String str = splitStrings[i];
+            for (String s : foundMatches) {
+                if (!selectWords) {
+                    if (str.contains(s)) {
+                        result += str;
+                        result += System.lineSeparator();
+                        if (printLastStrings) {
+                            for (int j = i + 1; j < i + optArg + 1; j++) {
+                                if (j >= 0 & j < splitStrings.length) {
+                                    result += splitStrings[j];
+                                    result += System.lineSeparator();
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    String[] splits = parser.splitByWhitespace(str);
+                    for (String st : splits) {
+                        if (s.equals(st)) {
+                            result += str;
+                            result += System.lineSeparator();
+                            if (printLastStrings) {
+                                for (int j = i + 1; j < i + optArg + 1; j++) {
+                                    if (j >= 0 & j < splitStrings.length) {
+                                        result += splitStrings[j];
+                                        result += System.lineSeparator();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         return result;
     }
 }
